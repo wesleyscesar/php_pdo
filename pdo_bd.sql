0931ab0ea7cfc87d624fe7b3775f09fdf381f187
wesleyscesar <wesley@criasoft.com.br>
2016-03-08 16:18:34 -0300

    Added files via upload

--- pdo_bd.sql
+++ pdo_bd.sql
@@ -1,3 +1,7 @@
+create database pdo_bd;
+
+use pdo_bd;
+
 create table alunos(
 	id int(5) auto_increment primary key,
 	nome varchar(255),
@@ -13,7 +17,7 @@ insert into alunos (nome,nota) values('Aluno6',6);
 insert into alunos (nome,nota) values('Aluno7',7);
 insert into alunos (nome,nota) values('Aluno8',8);
 insert into alunos (nome,nota) values('Aluno9',9);
-insert into alunos (nome,nota) values('Aluno10',5);
+insert into alunos (nome,nota) values('Aluno10',9);
 insert into alunos (nome,nota) values('Aluno11',6);
 insert into alunos (nome,nota) values('Aluno12',6);
 insert into alunos (nome,nota) values('Aluno13',8);
