4bd1e62983d0fb9d8dbf566e1baf629b3e0d6714
wesleyscesar <wesley@criasoft.com.br>
2016-03-08 16:16:26 -0300

    Added files via upload

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+$conexao = new \PDO("mysql:host=localhost;dbname=pdo","root","t2ic0l02");
---
+++ pdo_bd.sql
@@ -0,0 +1,27 @@
+create table alunos(
+	id int(5) auto_increment primary key,
+	nome varchar(255),
+	nota int()
+);
+
+insert into alunos (nome,nota) values('Aluno1',1);
+insert into alunos (nome,nota) values('Aluno2',2);
+insert into alunos (nome,nota) values('Aluno3',3);
+insert into alunos (nome,nota) values('Aluno4',4);
+insert into alunos (nome,nota) values('Aluno5',5);
+insert into alunos (nome,nota) values('Aluno6',6);
+insert into alunos (nome,nota) values('Aluno7',7);
+insert into alunos (nome,nota) values('Aluno8',8);
+insert into alunos (nome,nota) values('Aluno9',9);
+insert into alunos (nome,nota) values('Aluno10',5);
+insert into alunos (nome,nota) values('Aluno11',6);
+insert into alunos (nome,nota) values('Aluno12',6);
+insert into alunos (nome,nota) values('Aluno13',8);
+insert into alunos (nome,nota) values('Aluno14',9);
+insert into alunos (nome,nota) values('Aluno15',7);
+insert into alunos (nome,nota) values('Aluno16',1);
+insert into alunos (nome,nota) values('Aluno17',4);
+insert into alunos (nome,nota) values('Aluno18',3);
+insert into alunos (nome,nota) values('Aluno19',2);
+insert into alunos (nome,nota) values('Aluno20',1);
+
